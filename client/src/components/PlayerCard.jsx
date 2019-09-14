d4c289f6f04f59231c798d1559f23e42c69d684c
Mada Gower <gmgower@gmail.com>
2019-09-13 22:50:45 -0500

    Add semantic.

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Card, Icon} from 'semantic-ui-react'
 
 
 const PlayerCard = (props) => {
@@ -6,9 +7,17 @@ const PlayerCard = (props) => {
     const {player} = props
 return (
         <div className="player-card">
-            <h1>{player.name}</h1>
+        <Card key={player.id}>  
+            <Card.Content header={player.name} />
+            <Card.Content description={player.country} />
+            <Card.Content extra>
+              <Icon name='search' /> {player.searches} Searches
+            </Card.Content>
+            </Card>
+
+            {/* <h1>{player.name}</h1>
             <p>{player.country}</p>
-            <p>{player.searches}</p>
+            <p>{player.searches}</p> */}
         </div>
     )
 }
