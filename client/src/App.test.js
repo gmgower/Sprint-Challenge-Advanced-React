6720afe384d1e16c892766f4ef06cb5dcb426903
Mada Gower <gmgower@gmail.com>
2019-09-13 22:27:12 -0500

    Write test.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import {render} from '@testing-library/react';
+
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// import {scoreStrike} from './App'
+
+test('App renders without crashing', () => {
+ 
+  render(<App />)
+});
+
+
+test(' App header contains text', () => {
+  const { getByText } = render(<App />);
+  getByText(/World Cup/i);
 });
