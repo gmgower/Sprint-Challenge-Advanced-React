f2dfb53589dbcd6bdca5a09c71fd38e0b819cbf0
Mada Gower <gmgower@gmail.com>
2019-09-13 20:37:47 -0500

    Add axios, grab data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+  const [players, setPlayers] = useState([])
+
+
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log(res.data)
+      setPlayers(res.data);
+    })
+    .catch(error => {
+      console.log('Server Error', error)
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Women's World Cup</h1>
+
       </header>
     </div>
   );
